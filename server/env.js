69726d0b4c5ed40057e68ade63024ce85a8d4865
parijat <parijat1072005@gmail.com>
2026-03-15 14:40:27 +0530

    fix: use absolute path for dotenv config

--- server/env.js
+++ server/env.js
@@ -1,2 +1,8 @@
 import dotenv from 'dotenv';
-dotenv.config();
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, '.env') });
